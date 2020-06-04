4d1207429b39e9cea128be5f981818a9c1e5d3ae
realikechukwu <66019657+realikechukwu@users.noreply.github.com>
2020-06-04 16:59:26 +0100

    create a new file
    New-member

---
+++ scripts/ikechukwu-chukwudi.py
@@ -0,0 +1,9 @@
+#Name: Ikechukwu Chukwudi
+#Task: Stage 2 HNG Task
+
+name = "Ikechukwu Chukwudi"
+id = "HNG-XXXX"
+language = "python"
+email = "ikechukwu.chukwudi@gmail.com"
+
+print("Hello World, this is " + name + " with HNGi7 ID " + id + " and email " + email + " using " + language + " for stage 2 task")
